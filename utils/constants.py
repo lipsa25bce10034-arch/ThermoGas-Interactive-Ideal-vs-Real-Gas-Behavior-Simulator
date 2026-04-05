04e05c8502ee0d671b641da6bd8eb5c52903fedc
Lipsa Rani Panda <lipsa.25bce10034@vitbhopal.ac.in>
2026-04-05 13:20:20 +0530

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,18 @@
+"""
+This file contains all constant values used in the project
+"""
+
+# Universal Gas Constant (L·atm/mol·K)
+R = 0.0821
+
+# Default Temperature (K)
+DEFAULT_TEMPERATURE = 300
+
+# Default Pressure (atm)
+DEFAULT_PRESSURE = 1
+
+# Default Volume (L)
+DEFAULT_VOLUME = 1
+
+# Default Moles
+DEFAULT_MOLES = 1
