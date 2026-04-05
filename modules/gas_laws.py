e0b3685397e65f5b950b18295b77b7424d4b58fe
Lipsa Rani Panda <lipsa.25bce10034@vitbhopal.ac.in>
2026-04-05 13:01:50 +0530

    Implement gas laws functions in gas_laws.py

---
+++ modules/gas_laws.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+# Boyle's Law: P = k / V
+def boyles_law(V, k):
+    return k / V
+
+# Charles Law: V = k * T
+def charles_law(T, k):
+    return k * T
+
+# Gay-Lussac Law: P = k * T
+def gay_lussac_law(T, k):
+    return k * T
